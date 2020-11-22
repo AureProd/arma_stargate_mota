3b1514af8bcc00faf79dd62146acd2353f2912e2
JB HUET <jean-baptiste.huet@epitech.eu>
2020-11-22 02:32:35 +0100

    ajout illégalité sur les items

--- mission.Stargate_Impact_Map/config/item_config.cpp
+++ mission.Stargate_Impact_Map/config/item_config.cpp
@@ -5,10 +5,11 @@ class stargate_items
     {
         /* 
             { 
-                id / level / nom / lien image / type objets / poid / is militaire (1 = militaire / 0 = pas militaire) / prix / 
-                is tauri (1 = tauri / 0 = pas tauri / 2 = tauri + gouald) / 
-                (si nourriture ou boisson ou raisin -> valeur que ca te rend (en %)) 
-                (si medicament ou blessure -> partie du corp (0 = tout corp / 1 = tete / 2 = ventre / 3 = tete et ventre / 4 = bras / 5 = jambes))
+                id / level / nom / lien image / type objets / poid / is militaire (1 = militaire, 0 = pas militaire) / prix / 
+                is tauri (1 = tauri, 0 = pas tauri, 2 = tauri + gouald) / speciale / is illégal (1 = illégal, 0 = pas illégal)
+                |-> pour speciale :
+                    |-> si nourriture ou boisson ou raisin ou viande cuite ou poisson cuit -> valeur que ca te rend (en %)
+                    |-> si medicament ou blessure -> partie du corp (0 = tout corp / 1 = tete / 2 = ventre / 3 = tete et ventre / 4 = bras / 5 = jambes)
             }
         */ 
         /*  
@@ -26,48 +27,45 @@ class stargate_items
             poisson cuit --> 11
         */
         tableau_items[] = {
-			{1, 0, "bouteille", "pictures\items\bouteille.paa", 2, 1.5, 0, 10, 1, 30},
-            {2, 15, "gourde", "pictures\items\gourde.paa", 5, 3, 1, 70, 1, 50},
-            {3, 15, "micropur", "pictures\items\micropur.paa", 5, 0.5, 25, 0, 3},
-            {4, 15, "ration", "pictures\items\ration.paa", 1, 1, 1, 50, 1, 30},
-            {5, 0, "sandwich", "pictures\items\sandwich.paa", 1, 1, 0, 8, 1, 30},
-            {6, 0, "bandage", "pictures\items\bandage.paa", 3, 0.5, 0, 15, 1, 0},
-            {7, 0, "aspirine", "pictures\items\aspirine.paa", 4, 0.5, 0, 10, 1, 0},
-            {8, 0, "smecta", "pictures\items\smecta.paa", 4, 0.5, 0, 8, 1, 0},
-            //{11, 0, "bandage_goauld", "pictures\items\bandage.paa", 3, 0.5, 0, 0, 0},
-            //{12, 0, "aspirine_goauld", "pictures\items\aspirine.paa", 4, 0.5, 0, 0, 0},
-            //{13, 0, "smecta_goauld", "pictures\items\smecta.paa", 4, 0.5, 0, 0, 0},
-            {14, 0, "raisin", "pictures\items\raisin_baselvl0_20.paa", 6, 0.5, 0, 5, 0, 20},
-            {15, 20, "raisin", "pictures\items\raisin_mediocrelvl20_40.paa", 6, 0.5, 0, 45, 0, 40},
-            {16, 40, "raisin", "pictures\items\raisin_superieurlvl40_60.paa", 6, 0.5, 0, 75, 0, 60},
-            {17, 0, "tabregol", "pictures\items\potion_verte.paa", 4, 0.5, 0, 10, 0, 1},
-            {18, 0, "try'rid", "pictures\items\potion_rouge.paa", 4, 0.5, 0, 15, 0, 2},
-            {19, 0, "vuhran", "pictures\items\cataplasme-bleu.paa", 3, 0.5, 0, 8, 0, 3},
-            {20, 0, "shorkoak", "pictures\items\cataplasme-noir.paa", 3, 0.5, 0, 12, 0, 4},
-            {21, 0, "ocysed", "pictures\items\cataplasme-rouge.paa", 3, 0.5, 0, 17, 0, 5},
-            {22, 0, "mouton cru", "pictures\items\viande\raw_mouton.paa", 8, 1, 0, 5, 2, 15},
-            {23, 0, "mouton cuit", "pictures\items\viande\raw_mouton.paa", 9, 0.5, 0, 5, 2, 30},
-            {24, 0, "chevre crue", "pictures\items\viande\raw_chevre.paa", 8, 1, 0, 3, 2, 10},
-            {25, 0, "chevre cuite", "pictures\items\viande\raw_chevre.paa", 9, 0.5, 0, 5, 2, 20},
-            {26, 0, "poule crue", "pictures\items\viande\raw_cock.paa", 8, 1, 0, 2, 2, 8},
-            {27, 0, "poule cuite", "pictures\items\viande\raw_cock.paa", 9, 0.5, 0, 4, 2, 15},
-            {28, 0, "lapin cru", "pictures\items\viande\raw_hen.paa", 8, 1, 0, 1, 2, 5},
-            {29, 0, "lapin cuit", "pictures\items\viande\raw_hen.paa", 9, 0.5, 0, 3, 2, 10},
-            {30, 0, "couteau", "pictures\items\couteaux\couteau_taury.paa", 7, 3, 0, 15, 1, 0},
-            {31, 0, "couteau", "pictures\items\couteaux\couteau_goauld.paa", 7, 3, 0, 15, 0, 0},
-            {32, 0, "symbiote", "pictures\items\poissons\symbiote.paa", 10, 2, 0, 12, 2, 0},
-            {33, 0, "maquereau cru", "pictures\items\poissons\maquereau.paa", 10, 1, 0, 6, 2, 10},
-            {34, 0, "maquereau cuit", "pictures\items\poissons\maquereau.paa", 11, 0.5, 0, 8, 2, 35},
-            {35, 0, "mullet cru", "pictures\items\poissons\mullet.paa", 10, 1, 0, 5, 2, 8},
-            {36, 0, "mullet cuit", "pictures\items\poissons\mullet.paa", 11, 0.5, 0, 7, 2, 30},
-            {37, 0, "ornate cru", "pictures\items\poissons\ornate.paa", 10, 1, 0, 4, 2, 5},
-            {38, 0, "ornate cuit", "pictures\items\poissons\ornate.paa", 11, 0.5, 0, 7, 2, 25},
-            {39, 0, "poisson chat cru", "pictures\items\poissons\poisson_chat.paa", 10, 1, 0, 1, 2, 5},
-            {40, 0, "poisson chat cuit", "pictures\items\poissons\poisson_chat.paa", 11, 0.5, 0, 3, 2, 15},
-            {41, 0, "salema cru", "pictures\items\poissons\salema.paa", 10, 1, 0, 2, 2, 6},
-            {42, 0, "salema cuit", "pictures\items\poissons\salema.paa", 11, 0.5, 0, 4, 2, 20},
-            {43, 0, "thon cru", "pictures\items\poissons\thon.paa", 10, 1, 0, 3, 2, 7},
-            {44, 0, "thon cuit", "pictures\items\poissons\thon.paa", 11, 0.5, 0, 6, 2, 25}
+			{1, 0, "bouteille", "pictures\items\bouteille.paa", 2, 1.5, 0, 10, 1, 30, 0},
+            {2, 15, "gourde", "pictures\items\gourde.paa", 5, 3, 1, 70, 1, 50, 0},
+            {3, 15, "micropur", "pictures\items\micropur.paa", 5, 0.5, 25, 0, 3, 0},
+            {4, 15, "ration", "pictures\items\ration.paa", 1, 1, 1, 50, 1, 30, 0},
+            {5, 0, "sandwich", "pictures\items\sandwich.paa", 1, 1, 0, 8, 1, 30, 0},
+            {6, 0, "bandage", "pictures\items\bandage.paa", 3, 0.5, 0, 15, 1, 0, 0},
+            {7, 0, "aspirine", "pictures\items\aspirine.paa", 4, 0.5, 0, 10, 1, 0, 0},
+            {8, 0, "smecta", "pictures\items\smecta.paa", 4, 0.5, 0, 8, 1, 0, 0},
+            {14, 0, "raisin", "pictures\items\raisin_baselvl0_20.paa", 6, 0.5, 0, 5, 0, 20, 0},
+            {15, 20, "raisin", "pictures\items\raisin_mediocrelvl20_40.paa", 6, 0.5, 0, 45, 0, 40, 0},
+            {16, 40, "raisin", "pictures\items\raisin_superieurlvl40_60.paa", 6, 0.5, 0, 75, 0, 60, 0},
+            {17, 0, "tabregol", "pictures\items\potion_verte.paa", 4, 0.5, 0, 10, 0, 1, 0},
+            {18, 0, "try'rid", "pictures\items\potion_rouge.paa", 4, 0.5, 0, 15, 0, 2, 0},
+            {19, 0, "vuhran", "pictures\items\cataplasme-bleu.paa", 3, 0.5, 0, 8, 0, 3, 0},
+            {20, 0, "shorkoak", "pictures\items\cataplasme-noir.paa", 3, 0.5, 0, 12, 0, 4, 0},
+            {21, 0, "ocysed", "pictures\items\cataplasme-rouge.paa", 3, 0.5, 0, 17, 0, 5, 0},
+            {22, 0, "mouton cru", "pictures\items\viande\raw_mouton.paa", 8, 1, 0, 5, 2, 15, 0},
+            {23, 0, "mouton cuit", "pictures\items\viande\raw_mouton.paa", 9, 0.5, 0, 5, 2, 30, 0},
+            {24, 0, "chevre crue", "pictures\items\viande\raw_chevre.paa", 8, 1, 0, 3, 2, 10, 0},
+            {25, 0, "chevre cuite", "pictures\items\viande\raw_chevre.paa", 9, 0.5, 0, 5, 2, 20, 0},
+            {26, 0, "poule crue", "pictures\items\viande\raw_cock.paa", 8, 1, 0, 2, 2, 8, 0},
+            {27, 0, "poule cuite", "pictures\items\viande\raw_cock.paa", 9, 0.5, 0, 4, 2, 15, 0},
+            {28, 0, "lapin cru", "pictures\items\viande\raw_hen.paa", 8, 1, 0, 1, 2, 5, 0},
+            {29, 0, "lapin cuit", "pictures\items\viande\raw_hen.paa", 9, 0.5, 0, 3, 2, 10, 0},
+            {30, 0, "couteau", "pictures\items\couteaux\couteau_taury.paa", 7, 3, 0, 15, 1, 0, 0},
+            {31, 0, "couteau", "pictures\items\couteaux\couteau_goauld.paa", 7, 3, 0, 15, 0, 0, 0},
+            {32, 0, "symbiote", "pictures\items\poissons\symbiote.paa", 10, 2, 0, 12, 2, 0, 0},
+            {33, 0, "maquereau cru", "pictures\items\poissons\maquereau.paa", 10, 1, 0, 6, 2, 10, 0},
+            {34, 0, "maquereau cuit", "pictures\items\poissons\maquereau.paa", 11, 0.5, 0, 8, 2, 35, 0},
+            {35, 0, "mullet cru", "pictures\items\poissons\mullet.paa", 10, 1, 0, 5, 2, 8, 0},
+            {36, 0, "mullet cuit", "pictures\items\poissons\mullet.paa", 11, 0.5, 0, 7, 2, 30, 0},
+            {37, 0, "ornate cru", "pictures\items\poissons\ornate.paa", 10, 1, 0, 4, 2, 5, 0},
+            {38, 0, "ornate cuit", "pictures\items\poissons\ornate.paa", 11, 0.5, 0, 7, 2, 25, 0},
+            {39, 0, "poisson chat cru", "pictures\items\poissons\poisson_chat.paa", 10, 1, 0, 1, 2, 5, 0},
+            {40, 0, "poisson chat cuit", "pictures\items\poissons\poisson_chat.paa", 11, 0.5, 0, 3, 2, 15, 0},
+            {41, 0, "salema cru", "pictures\items\poissons\salema.paa", 10, 1, 0, 2, 2, 6, 0},
+            {42, 0, "salema cuit", "pictures\items\poissons\salema.paa", 11, 0.5, 0, 4, 2, 20, 0},
+            {43, 0, "thon cru", "pictures\items\poissons\thon.paa", 10, 1, 0, 3, 2, 7, 0},
+            {44, 0, "thon cuit", "pictures\items\poissons\thon.paa", 11, 0.5, 0, 6, 2, 25, 0}
 		};
 
         // { tableau d'ids des items / message de presentation de item }
